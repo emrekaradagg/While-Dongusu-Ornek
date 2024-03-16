8e64c77f5f7d63ead9ce5af23a16b27b48b27a07
emre <emre>
2024-03-17 00:10:46 +0300

    While dongusu

---
+++ app.js
@@ -0,0 +1,32 @@
+//1 den 10 a kadar yazdıralım
+
+// let sayac=1;
+
+// while(sayac<=10) {
+//     console.log(sayac);
+//     sayac++;
+// }
+
+// 1 den 10 a kadar tek çift yazdıralım
+
+// let a = 0;
+
+// while(a<=10) {
+//     if(a%2==0) {
+//         console.log("çift :"+ a);
+//     }else {
+//         console.log("tek :"+ a);
+//     }
+//     a++;
+// }
+
+
+let sayac =1;
+
+while(true) {
+    console.log(sayac);
+    if(sayac==7) {
+        break;
+    }
+    sayac++;
+}
